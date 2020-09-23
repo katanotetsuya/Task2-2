56430d4cf0ef13eceb09b9b59241c20d01ab3b9a
katanotetsuya <katanotetsuya0812@gmail.com>
2020-09-23 22:20:13 +0900

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
 end
